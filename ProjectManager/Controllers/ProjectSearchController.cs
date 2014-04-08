04827ca0f9c1d17df25b8fe957c09e0a3e2e0e9b
Jose Garcia <garciajd90@gmail.com>
2014-04-07 23:12:19 -0700

    base ProjectController and ProjectSearchController classes created

---
+++ ProjectManager/Controllers/ProjectSearchController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManager.Controllers
+{
+    public class ProjectSearchController : Controller
+    {
+        //
+        // GET: /ProjectSearch/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
