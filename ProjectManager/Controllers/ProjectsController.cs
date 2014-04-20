581e32ce826aa2b7b9962ffbf0f9f4fed8238fd3
Jose Garcia <garciajd90@gmail.com>
2014-04-20 03:16:04 -0700

    disallow access to other tenants' projects

--- ProjectManager/Controllers/ProjectsController.cs
+++ ProjectManager/Controllers/ProjectsController.cs
@@ -55,10 +55,6 @@ namespace ProjectManager.Controllers
         [HttpPost]
         public ActionResult Create(Project newProject)
         {
-            // create new project here
-            // you will need a form in Views/Projects/Create
-            // the 'name' of each input should be the same as the required Project fields
-            // ex: <input type="text" name="Name"> would be the input for Project.Name
             Project project = new Project();
             project.TenantId = Auth.GetCurrentUser().TenantId;
             project.ManagerId = Auth.GetCurrentUser().UserId;
@@ -73,7 +69,6 @@ namespace ProjectManager.Controllers
 
             }
 
-            // return user to Projects/Index after creation is done
             return RedirectToAction("Index");
         }
 
@@ -82,9 +77,6 @@ namespace ProjectManager.Controllers
 
         public ActionResult Edit(int id)
         {
-            // this function only returns the page for editing a project
-            // we'll need a seperate Edit(Project project) for actually editing a project
-
             if (!Auth.IsLoggedIn())
             {
                 return RedirectToAction("Login", "Home");
@@ -92,7 +84,7 @@ namespace ProjectManager.Controllers
 
             DataClassesDataContext db = new DataClassesDataContext();
             Project project = (from p in db.Projects
-                               where p.ProjectId == id
+                               where p.TenantId == Auth.GetCurrentUser().TenantId && p.ProjectId == id
                                select p).FirstOrDefault();
             // still need to match TenantId once we implement logins
 
