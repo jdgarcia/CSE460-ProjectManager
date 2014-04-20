1b9e706ced601c16f88a96d77125695fa072c9c4
jtadamec <jamestadamec@gmail.com>
2014-04-19 22:00:02 -0700

    Project Creation page

--- ProjectManager/Controllers/ProjectsController.cs
+++ ProjectManager/Controllers/ProjectsController.cs
@@ -55,6 +55,18 @@ namespace ProjectManager.Controllers
             // you will need a form in Views/Projects/Create
             // the 'name' of each input should be the same as the required Project fields
             // ex: <input type="text" name="Name"> would be the input for Project.Name
+            using (var db = new DataClassesDataContext())
+            {
+                Project project = new Project();
+                project.TenantId = Auth.GetCurrentUser().TenantId;
+                project.ManagerId = Auth.GetCurrentUser().UserId;
+                project.Name = newProject.Name;
+                project.Start = newProject.Start;
+                project.ExpectedEnd = newProject.ExpectedEnd;
+
+                project.Status = 1;
+
+            }
 
 
             // return user to Projects/Index after creation is done
