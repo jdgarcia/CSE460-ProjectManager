f1cd3114b0876dc3781fbfae9f5665e057f48dbf
Rachel Helmstetter <Rachel.Helmstetter@gmail.com>
2014-04-20 01:13:25 -0700

    Initial User view and creation

---
+++ ProjectManager/Controllers/UsersController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManager.Models;
+using ProjectManager.Utils;
+
+namespace ProjectManager.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Users/
+
+        public ActionResult Index()
+        {
+            if (!Auth.IsLoggedIn())
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            List<UserContext> users = new List<UserContext>();
+
+            using (var db = new DataClassesDataContext())
+            {
+                var result = (from u in db.Users
+                              where u.TenantId == Auth.GetCurrentUser().TenantId
+                              select u);
+                foreach (var user in result)
+                {
+                    users.Add(new UserContext(user));
+                }
+            }
+
+            return View(users);
+        }
+
+        //
+        // GET: /Projects/Create
+
+        public ActionResult Create()
+        {
+            if (!Auth.IsLoggedIn())
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            return View();
+        }
+
+        //
+        // POST: /Users/
+        [HttpPost]
+
+        public ActionResult Create(User newUser)
+        {
+            if (!Auth.GetCurrentUser().IsAdmin)
+            {
+                return View("You do not have permisions to do that");
+            }
+            
+            using (var db = new DataClassesDataContext())
+            {
+                User user = new User();
+                user.TenantId = Auth.GetCurrentUser().TenantId;
+                user.Username = newUser.Username;
+                user.Password = Auth.GetPasswordHash(newUser.Password);
+                user.RoleId = 5;    // default to GLOBAL.Employee for now
+
+                db.Users.InsertOnSubmit(user);
+                db.SubmitChanges();
+
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProjectManager/Models/UserContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManager.Models
+{
+    public class UserContext
+    {
+        public int TenantId { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public UserContext(User user)
+        {
+            this.TenantId = user.TenantId;
+            this.UserId = user.UserId;
+            this.Username = user.Username;
+            this.Password = user.Password;
+            this.Role = user.Role.Title;
+        }
+    }
+}
