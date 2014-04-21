70b03b9a70c1c302008e4cfca308112b2fae9ba8
Jose Garcia <garciajd90@gmail.com>
2014-04-21 07:26:20 -0700

    CustomTypesController

---
+++ ProjectManager/Controllers/CustomTypesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManager.Models;
+using ProjectManager.Utils;
+
+namespace ProjectManager.Controllers
+{
+    public class CustomTypesController : Controller
+    {
+        //
+        // GET: /CustomTypes/
+
+        public ActionResult Index()
+        {
+            if (!Auth.IsLoggedIn())
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            return View();
+        }
+
+    }
+}
